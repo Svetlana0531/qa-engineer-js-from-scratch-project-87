2c707b4ba8acb2e06191ed5bd8e9afea27b6dbc3
Svetlana Denisova <svetlanalevshina@mail.ru>
2026-09-10 14:53:19 +0300

    Add arguments and option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,6 +8,11 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'display help for command');
+  .helpOption('-h, --help', 'display help for command')
+
+  // Добавляем два обязательных позиционных аргумента
+  .arguments('<filepath1> <filepath2>')
+  // Добавляем опцию выбора формата со значением по умолчанию (например, 'stylish')
+  .option('-f, --format <type>', 'output format', 'stylish');
 
 program.parse(process.argv);
